a6d38a55f4dc1cae032d1032eaec9af27b454705
Saadkhalid4378 <109415996+Saadkhalid4378@users.noreply.github.com>
2023-02-07 22:22:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+hellowgreeeter.sayhellow();
+goodbyegreeter.sayoodbye();
+(function () {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+ var firstLetter = names[i].charAt(0).toLowerCase();
+ if (firstLetter === 'j') {
+    goodbyegreeter.sayhellow(names[i]);
+  } else {
+    hellowgreeeter.sayoodbye(names[i]);
+  }
+}
 
+})();
