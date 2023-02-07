f1ca2ff71d376ef3c6ac7aa5a0b137088e08bee9
Saadkhalid4378 <109415996+Saadkhalid4378@users.noreply.github.com>
2023-02-07 22:22:09 +0500

    Update hellow.js

--- hellow.js
+++ hellow.js
@@ -1,1 +1,11 @@
+(function (window) {
+    varhellowgreeter ={};
+    hellowgreeter.name = "name"
+    var greeting = "hellow";
+    hellowgreeter.sayhellow = function () {
+        console.log (greeting + hellowgreeter.name);
 
+    }
+    window.hellowgreeter = hellowgreeter ;
+
+})(window);
