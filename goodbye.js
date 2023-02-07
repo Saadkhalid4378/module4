3f153a0ae9d8539fada038901ea2384a9be4ae62
Saadkhalid4378 <109415996+Saadkhalid4378@users.noreply.github.com>
2023-02-07 22:21:13 +0500

    Update goodbye.js

--- goodbye.js
+++ goodbye.js
@@ -1,1 +1,11 @@
+(function (window) {
+    vargoodbyegreeter ={};
+    goodbyegreeter.name = "name"
+    var greeting = "goodbye";
+    goodbyegreeter.sayhellow = function () {
+        console.log (greeting + goodbye.name);
 
+    }
+    window.goodbyegreeter = goodbye ;
+
+})(window);
